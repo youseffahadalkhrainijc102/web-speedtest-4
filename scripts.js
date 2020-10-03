abb42fd94689c78ed332bb8c128a58414f7eec74
yousef <yousefalkhrainij@gmail.com>
2020-10-03 17:17:33 +0300

    gg
    gg

--- scripts.js
+++ scripts.js
@@ -6,3 +6,19 @@ let instructor = {
     numberOfRepos: 200,
   },
 };
+
+// menawer
+
+// javascript
+
+// careless10
+
+console.log(instructor.name)
+
+console.log(instructor.courses[0])
+
+console.log(instructor.githubAccount.username)
+
+
+alert( "😎 استاذ مناور أقوى مدرس ويب" Alert ليظر لك
+)
